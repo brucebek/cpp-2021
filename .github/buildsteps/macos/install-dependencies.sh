9180d3fffa4bec62f928a93f2e35b1b5cba8e7c1
Bruce Bek <bruce.bek@hotmail.com>
2022-01-05 18:49:42 +0700

    Test actions

--- .github/buildsteps/macos/install-dependencies.sh
+++ .github/buildsteps/macos/install-dependencies.sh
@@ -6,7 +6,3 @@ trap '[[ $BASH_COMMAND != echo* ]] && echo $BASH_COMMAND' DEBUG
 cd "$LAB_INDEX" || exit $?
 
 vcpkg --version || exit $?
-
-which vcpkg || exit $?
-
-cat  /usr/local/bin/vcpkg/scripts/buildsystems/vcpkg.cmake
