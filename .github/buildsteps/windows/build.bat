9c726ec41f4b31767ee9e4762aa6a7bb09274d5d
Bruce Bek <bruce.bek@hotmail.com>
2022-01-09 10:12:41 +0700

    Test actions

--- .github/buildsteps/windows/build.bat
+++ .github/buildsteps/windows/build.bat
@@ -7,7 +7,7 @@ cmake --version                                               || exit /b 1
 @echo.
 @echo ============================= BUILD x86_32 ==============================
 cmake -E rm -rf "x86_32"                                      || exit /b 1
-cmake -G "Visual Studio 16 2019" -A Win32 -B "x86_32" -S ".." || exit /b 1
+cmake -G "Visual Studio 16 2019" -A Win32 -B "x86_32" -S ".." -D "CMAKE_TOOLCHAIN_FILE=%GITHUB_WORKSPACE%/vcpkg/scripts/buildsystems/vcpkg.cmake" || exit /b 1
 @echo.
 @echo.                             x86_32 (Debug)
 cmake --build "x86_32" --config Debug                         || exit /b 1
@@ -24,7 +24,7 @@ cmake --build "x86_32" --config Release                       || exit /b 1
 
 @echo ============================= BUILD x86_64 ==============================
 cmake -E rm -rf "x86_64"                                      || exit /b 1
-cmake -G "Visual Studio 16 2019" -A x64 -B "x86_64" -S ".."   || exit /b 1
+cmake -G "Visual Studio 16 2019" -A x64 -B "x86_64" -S ".."  -D "CMAKE_TOOLCHAIN_FILE=%GITHUB_WORKSPACE%/vcpkg/scripts/buildsystems/vcpkg.cmake" || exit /b 1
 @echo.
 @echo.                             x86_64 (Debug)
 cmake --build "x86_64" --config Debug                         || exit /b 1
